09d4fb40d70ee95388978a82d7b6a84b847c8ea4
Edimilson <valeedimilson@gmail.com>
2023-12-31 00:31:01 -0300

    codigo reescrito

--- script.js
+++ script.js
@@ -1,103 +1,199 @@
-let jogoRodando = true;
 const fps = 24;
+let jogoRodando = false;
+
+const pontuacao = document.querySelector("#pontuacao");
+
+const orientacao = document.querySelector(".comandos");
 
 const tela = document.querySelector("#tela");
-const telaWidth = 400;
-const telaHeight = 400;
-const telaTamanhoBloco = 10;
-const telaWidthQuantidadeBloco = telaWidth / telaTamanhoBloco;
-const telaHeightQuantidadeBloco = telaHeight / telaTamanhoBloco;
+const telaTamanhoBloco = 8;
+const telaTotalQuadradosLargura = 40;
+const telaTotalQuadradosAltura = 60;
 
-tela.style.display = "grid";
-tela.style.backgroundColor = "darkgrey";
-tela.style.border = "1px solid black";
-tela.style.gridTemplateColumns = `repeat(${telaWidthQuantidadeBloco}, ${telaTamanhoBloco}px`;
-tela.style.gridTemplateRows = `repeat(${telaHeightQuantidadeBloco}, ${telaTamanhoBloco}px`;
+const pontos = [0, 0];
 
 const bolinha = document.querySelector("#bolinha");
-const bolinhaTamanho = 20;
-let bolinhaPosX = 0;
-let bolinhaPosY = 0;
+let bolinhaVelocidade = 1;
+let bolinhaDirecaoY = bolinhaVelocidade;
+let bolinhaDirecaoX = bolinhaVelocidade;
+const bolinhaTamanho = 2;
+//const bolinhaPosicao = [0, 0];
+const bolinhaPosicaoInicial = [
+  telaTotalQuadradosLargura / 2 - bolinhaTamanho / 2,
+  telaTotalQuadradosAltura / 4 - bolinhaTamanho / 2,
+];
+const bolinhaPosicao = Array.from(bolinhaPosicaoInicial);
 
-bolinha.style.backgroundColor = "black";
-bolinha.style.borderRadius = "100%";
-bolinha.style.width = `${bolinhaTamanho}px`;
-bolinha.style.height = `${bolinhaTamanho}px`;
+class jogador {
+  constructor(objeto, tamanhoX, tamanhoY, posicaoX, posicaoY) {
+    this.objeto = objeto;
+    this.tamanhoX = tamanhoX;
+    this.tamanhoY = tamanhoY;
+    this.posicaoX = posicaoX;
+    this.posicaoY = posicaoY;
+  }
+}
 
-let jogador1TamX = 100;
-let jogador1TamY = 20;
+const jogador1 = new jogador(document.querySelector("#jogador1"), 16, 2);
+jogador1.posicaoX = telaTotalQuadradosLargura / 2 - jogador1.tamanhoX / 2;
+jogador1.posicaoY = telaTotalQuadradosAltura - jogador1.tamanhoY - 1;
 
-let jogador1PosicaoX =
-  telaWidthQuantidadeBloco / 2 - jogador1TamX / 2 / telaTamanhoBloco;
-let jogador1PosicaoY = telaWidthQuantidadeBloco - 2;
+const jogador2 = new jogador(document.querySelector("#jogador2"), 16, 2);
+jogador2.posicaoX = telaTotalQuadradosLargura / 2 - jogador2.tamanhoX / 2;
+jogador2.posicaoY = jogador2.tamanhoY;
 
-console.log(jogador1PosicaoX);
+function desenhaPontuacao() {
+  const spanPontos = pontuacao.querySelectorAll("span");
+  spanPontos[0].innerText = pontos[0];
+  spanPontos[1].innerText = pontos[1];
+}
 
-const jogador1 = document.querySelector("#jogador1");
-jogador1.style.backgroundColor = "black";
-jogador1.style.width = `${jogador1TamX}px`;
-jogador1.style.height = `${jogador1TamY}px`;
-jogador1.style.gridColumn = jogador1PosicaoX;
-jogador1.style.gridRow = jogador1PosicaoY;
+function desenhaTela() {
+  if (jogoRodando == true) {
+    orientacao.style.display = "none";
+  } else {
+    orientacao.style.display = "block";
+  }
+  tela.style.gridTemplateColumns = `Repeat(${telaTotalQuadradosLargura}, ${telaTamanhoBloco}px)`;
+  tela.style.gridTemplateRows = `Repeat(${telaTotalQuadradosAltura}, ${telaTamanhoBloco}px)`;
+}
+
+function desenhaBolinha() {
+  bolinha.style.width = telaTamanhoBloco * bolinhaTamanho + "px";
+  bolinha.style.height = telaTamanhoBloco * bolinhaTamanho + "px";
+
+  bolinha.style.gridColumn = bolinhaPosicao[0];
+  bolinha.style.gridRow = bolinhaPosicao[1];
+}
+
+function desenhaJogador(jogador) {
+  jogador.objeto.style.width = telaTamanhoBloco * jogador.tamanhoX + "px";
+  jogador.objeto.style.height = telaTamanhoBloco * jogador.tamanhoY + "px";
+
+  jogador.objeto.style.gridColumn = jogador.posicaoX;
+  jogador.objeto.style.gridRow = jogador.posicaoY;
+}
+
+function direcaoAleatoria() {
+  return Math.round(Math.random()) * 1;
+}
+
+function checaColisao() {
+  let bolinhaPosX = bolinhaPosicao[0] + bolinhaTamanho / 2;
+
+  if (bolinhaPosicao[1] >= telaTotalQuadradosAltura) {
+    bolinhaPosicao[0] = bolinhaPosicaoInicial[0];
+    bolinhaPosicao[1] = bolinhaPosicaoInicial[1];
+    bolinhaDirecaoY = -bolinhaDirecaoY;
+
+    pontos[1] = pontos[1] + 1; //jogador2 ganhou
+    //jogoRodando = false;
+  } else if (bolinhaPosicao[1] <= 0) {
+    bolinhaPosicao[0] = bolinhaPosicaoInicial[0];
+    bolinhaPosicao[1] = bolinhaPosicaoInicial[1];
+    bolinhaDirecaoY = -bolinhaDirecaoY;
+
+    pontos[0] = pontos[0] + 1; //jogador1 ganhou
+
+    //jogoRodando = false;
+  } else {
+    if (bolinhaPosicao[1] + bolinhaTamanho == jogador1.posicaoY) {
+      if (
+        bolinhaPosX >= jogador1.posicaoX &&
+        bolinhaPosX <= jogador1.posicaoX + jogador1.tamanhoX
+      ) {
+        //console.log("colidiu com o jogador 1");
+        bolinhaDirecaoY = -bolinhaDirecaoY;
+        if (direcaoAleatoria == 1) {
+          bolinhaDirecaoX = -bolinhaDirecaoX;
+        }
+      }
+    }
+
+    if (bolinhaPosicao[1] - bolinhaTamanho == jogador2.posicaoY) {
+      if (
+        bolinhaPosX >= jogador2.posicaoX &&
+        bolinhaPosX <= jogador2.posicaoX + jogador2.tamanhoX
+      ) {
+        //console.log("colidiu com o jogador 2");
+        bolinhaDirecaoY = -bolinhaDirecaoY;
+
+        if (direcaoAleatoria == 1) {
+          bolinhaDirecaoX = -bolinhaDirecaoX;
+        }
+      }
+    }
+
+    if (bolinhaPosX >= telaTotalQuadradosLargura) {
+      bolinhaDirecaoX = -bolinhaDirecaoX;
+    } else if (bolinhaPosX <= 2) {
+      bolinhaDirecaoX = -bolinhaDirecaoX;
+    }
+  }
+}
 
 function moveBolinha() {
-  if (bolinhaPosX == telaHeightQuantidadeBloco - 1) {
-    //jogoRodando = false;
-    bolinhaPosX = 0;
-    bolinhaPosY = 0;
-    //console.log("perdeu");
-  } else {
-    bolinhaPosX++;
-    bolinhaPosY++;
-  }
-  bolinha.style.gridColumn = bolinhaPosX;
-  bolinha.style.gridRow = bolinhaPosY;
+  //console.log(direcaoAleatoria());
+
+  bolinhaPosicao[0] = bolinhaPosicao[0] + bolinhaDirecaoX;
+  bolinhaPosicao[1] = bolinhaPosicao[1] + bolinhaDirecaoY;
 }
 
-function desenhaJogador() {
-  document.querySelector("#jogador1").style.gridColumn = jogador1PosicaoX;
+function moveJogador(jogador, direcao) {
+  if (jogoRodando == true) {
+    if (direcao === "esquerda") {
+      jogador.posicaoX = jogador.posicaoX - 1;
+    } else if (direcao === "direita") {
+      jogador.posicaoX = jogador.posicaoX + 1;
+    }
+
+    if (jogador.posicaoX <= 1) {
+      jogador.posicaoX = 1;
+    }
+
+    if (jogador.posicaoX >= telaTotalQuadradosLargura - jogador.tamanhoX + 1) {
+      jogador.posicaoX = telaTotalQuadradosLargura - jogador.tamanhoX + 1;
+    }
+  }
 }
 
-function moveJogador(direcao) {
-  if (direcao == "esquerda") {
-    jogador1PosicaoX--;
-  } else if (direcao == "direita") {
-    jogador1PosicaoX++;
-  }
-
-  if (jogador1PosicaoX <= 1) {
-    jogador1PosicaoX = 1;
-  }
-
-  if (
-    jogador1PosicaoX >=
-    telaWidthQuantidadeBloco - jogador1TamX / telaTamanhoBloco + 1
-  ) {
-    jogador1PosicaoX =
-      telaWidthQuantidadeBloco - jogador1TamX / telaTamanhoBloco + 1;
-  }
-  console.log(`x: ${jogador1PosicaoX}`);
-}
-
-function handleKeyPress(event) {
+function lidaComEntrada(event) {
   switch (event.key) {
     case "ArrowLeft":
-      moveJogador("jogador1", "esquerda");
+      moveJogador(jogador1, "esquerda");
+      // moveJogador(jogador2, "esquerda", jogador2Posicao, jogador2TamanhoX);
       break;
     case "ArrowRight":
-      moveJogador("jogador1", "direita");
+      moveJogador(jogador1, "direita");
+      //moveJogador(jogador2, "direita", jogador2Posicao, jogador2TamanhoX);
+      break;
+
+    case "a":
+    case "A":
+      moveJogador(jogador2, "esquerda");
+      break;
+    case "d":
+    case "D":
+      moveJogador(jogador2, "direita");
+      break;
+    case "p":
+    case "P":
+      jogoRodando = jogoRodando ? false : true;
       break;
   }
 }
-document.addEventListener("keydown", handleKeyPress);
+document.addEventListener("keydown", lidaComEntrada);
 
-function loop() {
+setInterval(() => {
+  desenhaTela();
   if (jogoRodando == true) {
-    moveBolinha();
-    desenhaJogador();
-    setTimeout(loop, 1000 / fps);
-    //console.log("jogo rodando");
-  }
-}
+    desenhaPontuacao();
+    desenhaBolinha();
+    desenhaJogador(jogador1);
+    desenhaJogador(jogador2);
 
-loop(); // inicia loop do jogo
+    moveBolinha();
+
+    checaColisao();
+  }
+}, 1000 / fps);
